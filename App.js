67f491a3708864cb5337e63acc242119dd1eafaf
SetuCoder <58362876+SetuCoder@users.noreply.github.com>
2020-11-17 12:27:59 +0530

    Commit 1

---
+++ App.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { Text, View, TouchableOpacity, StyleSheet, Image, Vibration } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import {Linking} from 'react-native'
+
+export default class ScanScreen extends React.Component {
+    constructor(){
+      super();
+      this.state = {
+        hasCameraPermissions: null,
+        scanned: false,
+        scannedData: '',
+        buttonState: 'normal'
+      }
+    }
+
+    getCameraPermissions = async () =>{
+      const {status} = await Permissions.askAsync(Permissions.CAMERA);
+      
+      this.setState({
+        /*status === "granted" is true when user has granted permission
+          status === "granted" is false when user has not granted the permission
+        */
+        hasCameraPermissions: status === "granted",
+        buttonState: 'clicked',
+        scanned: false
+      });
+    }
+
+    handleBarCodeScanned = async({type, data})=>{
+      this.setState({
+        scanned: true,
+        scannedData: data,
+        buttonState: 'normal',
+      });
+    }
+
+    render() {
+      const hasCameraPermissions = this.state.hasCameraPermissions;
+      const scanned = this.state.scanned;
+      const buttonState = this.state.buttonState;
+
+      if (buttonState === "clicked" && hasCameraPermissions){
+        return(
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
+          />
+        );
+      }
+
+      else if (buttonState === "normal"){
+        return(
+          <View style={styles.container}>
+
+            <Image style={{marginBottom:15, marginLeft:8}} source={require('./assets/Icon.png')}/>
+
+          <Text style={styles.displayText}>{
+            hasCameraPermissions===true ? (Vibration.vibrate() ,this.state.scannedData) : "Please Allow Camera Permissions 📷"
+          }</Text>     
+
+          <TouchableOpacity
+            onPress={this.getCameraPermissions}
+            style={styles.scanButton}>
+            <Text style={styles.buttonText}>Scan QR Code</Text>
+          </TouchableOpacity>
+        </View>
+        );
+      }
+    }
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    displayText:{
+      fontSize: 15,
+      textDecorationLine: 'underline'
+    },
+    scanButton:{
+      backgroundColor: '#0EABC0',
+      padding: 10,
+      margin: 10
+    },
+    buttonText:{
+      fontSize: 20,
+    }
+  });
